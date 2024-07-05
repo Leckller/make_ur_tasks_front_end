318f62338935b951609908b27b6b3202e5963209
Gusttavo Ruy <7leckler@gmail.com>
2024-07-05 15:10:38 -0300

    feat: edit, remove, finish actions

--- src/components/TaskComponent.tsx
+++ src/components/TaskComponent.tsx
@@ -1,28 +1,32 @@
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 import { useAppDispatch } from '../hooks/reduxHooks';
-import { openEdit, viewTask } from '../redux/Reducers/Tasks';
+import { deleteTask, openEdit, toggleTask, viewTask } from '../redux/Reducers/Tasks';
 import { Task } from '../types';
 
 function TaskComponent({ task }: { task: Task }) {
   const dispatch = useAppDispatch();
   return (
-    <div className="flex flex-row gap-3">
+    <div className="flex flex-row gap-3 items-center">
+      <div className={ `${task.completed ? 'bg-green-500' : 'bg-red-500'} w-5 h-5` } />
       <button
         onClick={ () => {
           dispatch(openEdit({ type: 'view', bool: 1 }));
           dispatch(viewTask(task));
         } }
       >
-        <h2>{task.taskName}</h2>
+        <p>{task.taskName}</p>
       </button>
-      <button>
+      <button onClick={ () => dispatch(toggleTask(task)) }>
         Concluir Task
       </button>
-      <button>
+      <button onClick={ () => dispatch(deleteTask(task)) }>
         Remove Task
       </button>
       <button
-        onClick={ () => dispatch(openEdit({ type: 'edit', bool: 1 })) }
+        onClick={ () => {
+          dispatch(openEdit({ type: 'edit', bool: 1 }));
+          dispatch(viewTask(task));
+        } }
       >
         Edit Task
       </button>
