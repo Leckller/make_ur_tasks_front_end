ba615a44196d419768e09033559f2a58b6ffc1f4
Gusttavo Ruy <7leckler@gmail.com>
2024-07-05 22:53:10 -0300

    feat: fiz uma graça no pavao

--- src/components/TaskComponent.tsx
+++ src/components/TaskComponent.tsx
@@ -1,4 +1,6 @@
 /* eslint-disable jsx-a11y/no-static-element-interactions */
+import { FaRegTrashAlt } from 'react-icons/fa';
+import { BsPencil } from 'react-icons/bs';
 import { useAppDispatch } from '../hooks/reduxHooks';
 import { deleteTask, openEdit, toggleTask, viewTask } from '../redux/Reducers/Tasks';
 import { Task } from '../types';
@@ -6,9 +8,18 @@ import { Task } from '../types';
 function TaskComponent({ task }: { task: Task }) {
   const dispatch = useAppDispatch();
   return (
-    <div className="flex flex-row gap-3 items-center">
-      <div className={ `${task.completed ? 'bg-green-500' : 'bg-red-500'} w-5 h-5` } />
+    <div
+      className="flex flex-row gap-3 items-center flex-nowrap w-[90%]
+      border-4 border-black rounded-md p-2 max-w-[500px]"
+    >
+      <input
+        type="checkbox"
+        checked={ task.completed }
+        onChange={ () => dispatch(toggleTask(task)) }
+        className={ `${task.completed ? 'bg-green-500' : 'bg-red-500'} w-5 h-5` }
+      />
       <button
+        className="font-semibold w-[80%] break-words"
         onClick={ () => {
           dispatch(openEdit({ type: 'view', bool: 1 }));
           dispatch(viewTask(task));
@@ -16,20 +27,14 @@ function TaskComponent({ task }: { task: Task }) {
       >
         <p>{task.taskName}</p>
       </button>
-      <button onClick={ () => dispatch(toggleTask(task)) }>
-        Concluir Task
-      </button>
-      <button onClick={ () => dispatch(deleteTask(task)) }>
-        Remove Task
-      </button>
-      <button
+
+      <FaRegTrashAlt onClick={ () => dispatch(deleteTask(task)) } />
+      <BsPencil
         onClick={ () => {
           dispatch(openEdit({ type: 'edit', bool: 1 }));
           dispatch(viewTask(task));
         } }
-      >
-        Edit Task
-      </button>
+      />
     </div>
   );
 }
