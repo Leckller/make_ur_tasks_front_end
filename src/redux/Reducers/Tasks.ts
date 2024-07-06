c6a8031cf7094612a649b50517417fc965198544
Gusttavo Ruy <7leckler@gmail.com>
2024-07-05 22:15:54 -0300

    fix: lint error for vercel deploy

--- src/redux/Reducers/Tasks.ts
+++ src/redux/Reducers/Tasks.ts
@@ -1,7 +1,7 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
 import type { RootState } from '../store';
-import type { TaskFields, PopupTypes, Task, User } from '../../types';
+import type { TaskFields, PopupTypes, Task } from '../../types';
 import { LocalSaves } from '../../components/Classes/Saves';
 import { DataBase } from '../../service/Server';
 import TaskClass from '../../components/Classes/TaskClass';
