36a808cb658191e52f2206eaa31bf24536cfef07
ruy <7leckler@gmail.com>
2025-03-16 13:17:31 -0300

    feat: responsividade + novos estilos

--- src/components/TaskForm.vue
+++ src/components/TaskForm.vue
@@ -1,12 +1,12 @@
 <template>
 
-  <main>
+  <main id="main-task-form">
     <form id="task-form">
       <label for="task">Task:</label>
       <input minlength="2" v-model="taskInput" type="text" id="task" name="task" required>
       <button @click="addTask($event)" type="submit">Add Task</button>
     </form>
-    <section>
+    <section id="tasks-section">
 
       <TaskDetails
         v-for="task
@@ -64,3 +64,69 @@ export default {
 }
 
 </script>
+
+<style  scoped>
+
+  #main-task-form {
+    display: flex;
+    flex-direction: column;
+    gap: 16px;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 100%;
+  }
+
+  #tasks-section {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 16px;
+    width: 100%;
+  }
+
+  #task-form {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 1rem;
+    border: 1px solid #ccc;
+    gap: 16px;
+    max-width: 350px;
+    width: 90%;
+  }
+
+  #task-form button {
+    background-color: #0f0;
+    color: #fff;
+    border: none;
+    padding: .5rem 1rem;
+    cursor: pointer;
+    transition: all .3s;
+  }
+
+  #task-form button:hover {
+    background-color: #0f5;
+    transform: scale(1.05);
+  }
+
+  #task-form input {
+    max-width: 200px;
+  }
+
+  @media (max-width: 350px) {
+    #task-form {
+      flex-wrap: wrap;
+    }
+    #task-form input {
+      width: 100%;
+    }
+
+    button {
+      width: 100%;
+    }
+  }
+
+
+</style>
